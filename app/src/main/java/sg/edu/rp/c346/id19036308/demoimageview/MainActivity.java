d315ea083943b35b931243e048f4b7e487eea356
19036308 <19036308@myrp.edu.sg>
2021-04-22 12:07:52 +0800

    done

--- app/src/main/java/sg/edu/rp/c346/id19036308/demoimageview/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19036308/demoimageview/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     //Declare the ImageView object called ivDay2
-    ImageView ivDay2;
+    ImageView ivDay2,ivDay3, ivDay4, ivDay5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +18,20 @@ public class MainActivity extends AppCompatActivity {
         ivDay2 = (ImageView) findViewById(R.id.imageView2);
         // Set the image
         ivDay2.setImageResource(R.drawable.day2);
+        // Get the ImageView obj and assign to ivDay2
+        ivDay2 = (ImageView) findViewById(R.id.imageView3);
+        // Set the image
+        ivDay2.setImageResource(R.drawable.day3);
+
+        // Get the ImageView obj and assign to ivDay2
+        ivDay2 = (ImageView) findViewById(R.id.imageView4);
+        // Set the image
+        ivDay2.setImageResource(R.drawable.day4);
+
+        // Get the ImageView obj and assign to ivDay2
+        ivDay2 = (ImageView) findViewById(R.id.imageView5);
+        // Set the image
+        ivDay2.setImageResource(R.drawable.day5);
 
     }
 }
